3821a03ed54982b7ae6d0f707212be8644e953d5
Aniket <aniketparida158@gmail.com>
2024-08-12 16:18:35 +0530

    substraction of the class

--- Map.js
+++ Map.js
@@ -1,13 +1,13 @@
-class number{
+class NumberClass{
     constructor(element1, element2){
         this.element1 = element1;
         this.element2 = element2
     }
-    totalnumber(){
-        return this.element1+this.element2;
+    substaction(){
+        return this.element1 - this.element2;
     }
 }
-const element1 = 20;
-const element2 = 10;
-const instance = new number(element1, element2);
-console.log(instance.totalnumber());
+const element1 = 50;
+const element2 = 30;
+const totalnumber = new NumberClass(element1, element2);
+console.log(totalnumber.substaction());
