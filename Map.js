50bf2e856c13c58cc152b1beb2d7d6f9f040b84e
Aniket <aniketparida158@gmail.com>
2024-08-12 15:46:03 +0530

    output the average using the class fuction

--- Map.js
+++ Map.js
@@ -1,28 +1,12 @@
-//class = (ES6 feature) provides a more structured and clear eay to work with object to traditionaol constructur function.
-// class product{
-//     constructor(model, price){
-//         this.model = model,
-//         this.price = price
-//     };
-//     this.dispayproduct=function(percentage){
-//       return percentage(this.price/percentage*100)
-//     }
-// }
-// const percentage = 10;
-// const persion1 = new product("t-shirt", 100);
-// console.log(persion1.price);
-
-class Product {
-    constructor(model, price) {
-        this.model = model;
-        this.price = price;
+class Total{
+    constructor(firstnumber, secondnumber, thirdnumber){
+        this.firstnumber = firstnumber,
+        this.secondnumber = secondnumber,
+        this.thirdnumber = thirdnumber
     }
-
-    displayProduct(percentage) {
-        return this.price - (this.price * percentage / 100);
+    averagenumber(){
+        return ((this.firstnumber+this.secondnumber+this.thirdnumber)/3).toFixed(2);
     }
 }
-
-const percentage = 10;
-const person1 = new Product("t-shirt", 100);
-console.log(person1.displayProduct(percentage)); // Will display the price after applying the discount percentage
+const total1 = new Total(23, 22, 10);
+console.log(total1.averagenumber());
