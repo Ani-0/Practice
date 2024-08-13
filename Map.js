4a5276f2ffc71004b9854a3ef8640ced154a4aa8
Aniket <aniketparida158@gmail.com>
2024-08-13 22:54:37 +0530

    New

--- Map.js
+++ Map.js
@@ -1,34 +1,9 @@
-//getter - Getter method are spacial method that makes a property readable.
-//setter - setter method are spacial method that makes a property wrighting.
+function output(){
+    let conuntnumber = "Hello";
 
-class value{
-    constructor(height, width){
-        this.height = height;
-        this.width = width
+      function inner(){
+       console.log(conuntnumber);
     }
-    set width(newwidth){
-        if(newwidth > 0){
-            this._width = newwidth;
-        }
-        else{
-            console.error("Sorry you have not select positive number");
-        }
-    }
-    set height(newheight){
-        if(newheight > 0){
-            this._height = newheight; 
-        }
-        else{
-            console.error("Sorry you have not select positive number");
-        }
-    }
-    get width(){
-        return this._width;
-    }
-    get height(){
-        return this._height
-    }
+    inner();
 }
-const property = new value(100, 50);
-console.log(property.width);
-console.log(property._height);
+output();
