630e780e07077101d923c6b325cc7b428ff4f9d4
Aniket <aniketparida158@gmail.com>
2024-08-14 18:25:31 +0530

    DOM SELECTER

--- Practice.js
+++ Practice.js
@@ -1,33 +1,19 @@
-//Number guessing Game;
-const min = 1;
-const max = 100;
-const Answer = Math.floor (Math.random()*(max - min + 1)) + min; 
+//1.document.getElementClassName.
 
-let attempts = 0;
-let guess;
-let runtime = true;
+/*const fruits = document.getElementsByClassName("Fruits");
+fruits[0].style.backgroundColor = "Yellow";*/
 
-while(runtime){
-    guess = window.prompt(`Guess a number between ${min} -${max}`);
-    guess = Number(guess);
-    
-    if(isNaN(guess)){
-        window.alert("Enter a valid Number: ")
-    }
-    else if(guess < min || guess > max){
-        window.alert("guess the valid Number");
-    }
-    else{
-        attempts = 0;
-        if(Answer > guess){
-            window.alert("TOO LOW! NUMBER");
-        }
-        else if(Answer < guess){
-            window.alert("TOO HEIGH! NUMBER");
-        }
-        else{
-            window.alert(`congratulation you have select the currect ${Answer} Number`);
-            runtime = false;
-        }
-    }
-}
+//2.document.getElementTagName.
+
+/*const lielement = document.getElementsByTagName("li");
+lielement[0].style.backgroundColor = "lightgreen";*/
+
+//3.document.querySelector.
+
+const fruit = document.querySelector(".Fruits")
+fruit.style.backgroundColor = "yellow";
+
+//4.document.querySelectorAll.
+
+// const element = document.querySelectorAll(".Fruits");
+// element[0].style.backgroundColor = "Yellow";
